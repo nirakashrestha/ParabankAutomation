7ef33c25c5f0eae322ec113a2475e6aafc76bcf1
Nirakash Shrestha <nirakash.shrestha@gmail.com>
2025-11-17 21:03:12 -0500

    isolate test data

--- tests/LoginTest.spec.ts
+++ tests/LoginTest.spec.ts
@@ -1,13 +1,15 @@
 import test, { expect, Locator } from "@playwright/test";
 import { PageProvider } from "../helpers/PageProvider";
+import DataSet from "../data/loginData.json";
 
 
 let pages: PageProvider;
-const userName = "ecarter_test8";
+const loginData: any = JSON.parse(JSON.stringify(DataSet));
 
 
 test.beforeEach(async ({ page }) => {
      pages = new PageProvider(page);
+
 
      //visit parabankwebsite
      await page.goto("parabank/about.htm");
@@ -24,17 +26,17 @@ test('Register New User', async ({ page }) => {
 
      //fill user details   
 
-     await pages.registerPage.firstName.fill('Emily');
-     await pages.registerPage.lastName.fill('Carter');
-     await pages.registerPage.street.fill('742 Evergreen Terrace');
-     await pages.registerPage.city.fill('Springfield');
-     await pages.registerPage.state.fill('IL');
-     await pages.registerPage.zipCode.fill('62704');
-     await pages.registerPage.phoneNumber.fill('2175559382');
-     await pages.registerPage.ssn.fill('123456789');
-     await pages.registerPage.userName.fill(userName);
-     await pages.registerPage.password.fill('Test@1234');
-     await pages.registerPage.repeatedPassword.fill('Test@1234');
+     await pages.registerPage.firstName.fill(loginData.firstName);
+     await pages.registerPage.lastName.fill(loginData.lastName);
+     await pages.registerPage.street.fill(loginData.street);
+     await pages.registerPage.city.fill(loginData.city);
+     await pages.registerPage.state.fill(loginData.state);
+     await pages.registerPage.zipCode.fill(loginData.zipCode);
+     await pages.registerPage.phoneNumber.fill(loginData.phoneNumber);
+     await pages.registerPage.ssn.fill(loginData.ssn);
+     await pages.registerPage.userName.fill(loginData.userName);
+     await pages.registerPage.password.fill(loginData.password);
+     await pages.registerPage.repeatedPassword.fill(loginData.repeatedPassword);
      await pages.registerPage.register.click();
 
 
@@ -42,7 +44,7 @@ test('Register New User', async ({ page }) => {
      await page.waitForSelector('h1', { state: 'visible', timeout: 1000 });
 
      //assertion     
-     await expect(pages.registerPage.welcomeUser).toHaveText("Welcome " + userName);
+     await expect(pages.registerPage.welcomeUser).toHaveText("Welcome " + loginData.userName);
      await expect(pages.registerPage.accountCreateMsg).toHaveText("Your account was created successfully. You are now logged in.");
 
 });
@@ -50,8 +52,8 @@ test('Register New User', async ({ page }) => {
 
 test('Login to Parabank', async ({ page }) => {     
 
-     await pages.loginPage.user.fill(userName);
-     await pages.loginPage.password.fill("Test@1234");
+     await pages.loginPage.user.fill(loginData.userName);
+     await pages.loginPage.password.fill(loginData.password);
      await pages.loginPage.login.click();
 
      await expect(page).toHaveTitle("ParaBank | Accounts Overview");
